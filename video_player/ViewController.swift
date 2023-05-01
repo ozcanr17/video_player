b8e9c7a937914aa3512d230394c606488a1df595
ozcanr17 <ridvanozcan7@gmail.com>
2023-05-01 17:24:03 +0300

    Initial commit

---
+++ video_player/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  video_player
+//
+//  Created by Rıdvan on 20.04.2023.
+
+import UIKit
+import AVKit
+import AVFoundation
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        playVideo()
+    }
+    
+    private func playVideo() {
+        guard let path  = Bundle.main.path(forResource: "testvideo", ofType: "mp4") else {
+            debugPrint("testvideo.mp4 not found.")
+            return
+        }
+        let player = AVPlayer(url: URL(fileURLWithPath: path))
+        let playerController = AVPlayerViewController()
+        playerController.player = player
+        present(playerController, animated: true) { player.play() }
+    }
+}
+
